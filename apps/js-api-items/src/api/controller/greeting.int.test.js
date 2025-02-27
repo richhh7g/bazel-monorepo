67b00bd19c528d46e77b7afd72643fc7513cde62
Richhh7g <richhh7g@protonmail.com>
2025-02-27 03:20:19 -0300

    test(js): changes in greeting test

--- apps/js-api-items/src/api/controller/greeting.int.test.js
+++ apps/js-api-items/src/api/controller/greeting.int.test.js
@@ -1,6 +1,6 @@
 import request from "supertest";
 
-describe("Test Integration | RootController - greeting endpoint (GET)", () => {
+describe("Test Integration | RootController - greeting(GET)", () => {
   it('should respond with a status code 200 and message "Hello World"', async () => {
     const response = await request(global.server).get("/v1");
 
